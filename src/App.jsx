859017efb2d8028662da21cfc44d2dc4f72cbd34
nagymedo25 <nagymedo25@gmail.com>
2025-12-01 17:00:20 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-red-600 display-6">
+        Hello Vite + React + Tailwind!
+      </h1>
+    </div>
+  );
+}
